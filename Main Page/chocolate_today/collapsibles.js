c537796cfebdd8f70b4e07e620cd36113a9c898d
Zhabata <nevena.dimitrova2021@gmail.com>
2024-05-19 17:33:45 +0300

    make_at_home_chocolate_today
    everything ready

--- Main Page/chocolate_today/collapsibles.js
+++ Main Page/chocolate_today/collapsibles.js
@@ -5,7 +5,6 @@ for (i = 0; i < coll.length; i++) {
   console.log(i);
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    console.log("active")
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
