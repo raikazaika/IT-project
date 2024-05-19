0e5cb3a434351f9711dbf8ad624f608a8a83afd5
Zhabata <nevena.dimitrova2021@gmail.com>
2024-05-19 14:27:31 +0300

    base of chocolate today
    made the base for collapsIbels

---
+++ Main Page/chocolate_today/collapsibles.js
@@ -0,0 +1,18 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  console.log(i);
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    console.log("active")
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      console.log("none");
+    } else {
+      content.style.display = "block";
+      console.log("block");
+    }
+  });
+}
